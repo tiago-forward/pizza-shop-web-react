e746d6d4892be437032b86e0d9a00a81e601684b
tiagoforward <devtiagoforward@gmail.com>
2024-08-14 11:16:23 -0300

    added application layouts

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,19 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
 import { Dashbord } from './pages/app/dashbord'
 import { SignIn } from './pages/app/sign-in'
 
 export const router = createBrowserRouter([
-  { path: '/', element: <Dashbord /> },
-  { path: '/sign-in', element: <SignIn /> },
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [{ path: '/', element: <Dashbord /> }],
+  },
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [{ path: '/sign-in', element: <SignIn /> }],
+  },
 ])
